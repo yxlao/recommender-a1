7448e32774d8b5d7bb3370b35c8b30ccae0a9fa5
Yixing Lao <linuxthink@gmail.com>
2015-11-09 14:59:33 +0000

    kick off 64 grid again, 35 jobs

--- helpful_grid_search.py
+++ helpful_grid_search.py
@@ -200,18 +200,11 @@ pickle.dump((all_xs, all_ys, all_weights),
 print('dataset prepared')
 
 # set grid search param
-param_grid = {'learning_rate': [0.01, 0.005],
-              'max_depth': [4],
-              'min_samples_leaf': [9],
-              'max_features': [0.5],
-              'subsample': [0.3]
-              }
-
-param_grid = {'learning_rate': [0.01],
-              'max_depth': [4],
-              'min_samples_leaf': [9],
-              'max_features': [0.5],
-              'subsample': [0.3]
+param_grid = {'learning_rate': [0.1, 0.05, 0.01, 0.005],
+              'max_depth': [4, 6],
+              'min_samples_leaf': [3, 9],
+              'max_features': [0.1, 0.5],
+              'subsample': [0.1, 0.3]
               }
 
 # init regressor
@@ -220,8 +213,8 @@ regressor = GradientBoostingRegressor(n_estimators=MAX_ITER,
                                       verbose=1)
 
 # grid search
-grid_searcher = GridSearchCV(regressor, param_grid, verbose=1, n_jobs=3)
-grid_searcher.fit(all_xs[:3000], all_ys[:3000])
+grid_searcher = GridSearchCV(regressor, param_grid, verbose=1, n_jobs=35)
+grid_searcher.fit(all_xs, all_ys)
 
 # print best params
 opt_params = grid_searcher.best_params_
@@ -230,12 +223,11 @@ print(opt_params)
 # store optimal regressor
 opt_regressor = grid_searcher.best_estimator_
 
-opt_regressor_name = "opt_%s_%s_%s_%s_%s_%s" % (3000,
-                                                opt_params['learning_rate'],
-                                                opt_params['max_depth'],
-                                                opt_params['min_samples_leaf'],
-                                                opt_params['max_features'],
-                                                opt_params['subsample'])
+opt_regressor_name = "opt_%s_%s_%s_%s_%s" % (opt_params['learning_rate'],
+                                             opt_params['max_depth'],
+                                             opt_params['min_samples_leaf'],
+                                             opt_params['max_features'],
+                                             opt_params['subsample'])
 pickle.dump(opt_regressor, open(opt_regressor_name + '.pickle', 'wb'),
             protocol=pickle.HIGHEST_PROTOCOL)
 opt_regressor = pickle.load(open(opt_regressor_name + '.pickle', 'rb'))
