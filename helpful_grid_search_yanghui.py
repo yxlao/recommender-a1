64b7dec90e8199dde20e922e67a4d8058b7b335b
Yixing Lao <linuxthink@gmail.com>
2015-11-10 07:42:34 -0800

    reduce yanghui load to half

--- helpful_grid_search_yanghui.py
+++ helpful_grid_search_yanghui.py
@@ -263,7 +263,7 @@ param_grid = {'learning_rate': [0.1, 0.05, 0.01, 0.005],
               'max_depth': [4, 6],
               'min_samples_leaf': [3, 9],
               'max_features': [0.1, 0.5],
-              'subsample': [0.1, 0.3]
+              'subsample': [0.1]
               }
 
 # init regressor
