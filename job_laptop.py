5d0eb459a76d01383c665c0359399a5b2d575c8c
Yixing Lao <linuxthink@gmail.com>
2015-11-13 23:43:48 -0800

    add laptop's job

---
+++ job_laptop.py
@@ -0,0 +1,16 @@
+from helpful_grid_search import HelpfulGridSearcher
+
+# set params
+param_grid = {'learning_rate': [0.1, 0.05, 0.02, 0.01],
+              'max_depth': [4],
+              'min_samples_leaf': [3, 7, 9, 13],
+              'max_features': [0.5],
+              'subsample': [0.3]
+              }
+
+# execute
+searcher = HelpfulGridSearcher(param_grid=param_grid,
+                               n_estimators=3000,
+                               n_jobs=4,
+                               apply_weights=False)
+opt_regressor, opt_params = searcher.run()
