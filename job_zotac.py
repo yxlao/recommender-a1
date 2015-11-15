364c61b9dddafddaaac64710bdc850806caea780
Yixing Lao <linuxthink@gmail.com>
2015-11-15 03:52:58 -0800

    max feature try 0.4, 0.6

--- job_zotac.py
+++ job_zotac.py
@@ -1,16 +1,16 @@
 from helpful_grid_search import HelpfulGridSearcher
 
 # set params
-param_grid = {'learning_rate': [0.1, 0.05, 0.02, 0.01],
-              'max_depth': [4],
+param_grid = {'learning_rate': [0.2, 0.15, 0.1, 0.05],
+              'max_depth': [6],
               'min_samples_leaf': [9],
-              'max_features': [0.5],
-              'subsample': [0.1, 0.3, 0.5]
+              'max_features': [0.4, 0.6],
+              'subsample': [0.4, 0.5, 0.7]
               }
 
 # execute
 searcher = HelpfulGridSearcher(param_grid=param_grid,
                                n_estimators=3000,
-                               n_jobs=4,
+                               n_jobs=2,
                                apply_weights=False)
 opt_regressor, opt_params = searcher.run()
